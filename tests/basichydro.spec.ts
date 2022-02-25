8483bf005fe95e67c16ea76dbfb6c3694b81df81
kovács dani <kovacs.daniel@avander.hu>
2022-02-25 16:56:31 +0100

    fix

--- tests/basichydro.spec.ts
+++ tests/basichydro.spec.ts
@@ -28,16 +28,20 @@ test.describe("my first test suite", () => {
 
   test('Add new injury free event', async ({ page }) => {
 
-    await page.locator(".side-panel-content").waitFor()
-    /*await page.screenshot({ path: 'homepage1.png' });
+    await page.pause()
+    //await page.locator(".side-panel-content").waitFor()
+    await page.screenshot({ path: 'homepage1.png' });
     await page.locator("#i0116").type("ImsTestGlobalAdmin1@avander.hu")
     await page.keyboard.press("Enter");
 
     await page.locator("#i0118").type("123ims456!")
     await page.keyboard.press("Enter");
 
+    await page.locator('text=No')
+    page.locator('text=No').click()
+
     const ghostCard = await page.locator(".side-panel-content")
-    await ghostCard.screenshot({ path: 'side-panel-content.png' })*/
+    await ghostCard.screenshot({ path: 'side-panel-content.png' })
   })
 })
 
