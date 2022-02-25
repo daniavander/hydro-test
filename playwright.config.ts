7b1cf3803903c067aab67e9ec2c8eda33b3368f7
kovács dani <kovacs.daniel@avander.hu>
2022-02-25 15:42:17 +0100

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,13 @@ expect.extend(matchers)
 
 const config: PlaywrightTestConfig = {
 
-  globalTimeout: 100000, // Maximum time the whole test suite can run,
-  timeout: 100000,        // Timeout for each test
+  globalTimeout: 20000, // Maximum time the whole test suite can run,
+  timeout: 20000,        // Timeout for each test
   retries: 0,  //how many times re test the failed
   // See: https://playwright.dev/docs/test-reporters/
   //reporter: process.env.CI ? 'dot' : 'list',
-  reporter: './my-awesome-reporter.ts',
-  //reporter: 'html',
+  //reporter: './my-awesome-reporter.ts',
+  reporter: 'html',
   // See: https://playwright.dev/docs/api/class-testconfig
   outputDir: 'test-results/',
   //just the wanted folder will be run
