93c960759a5e7b96ccefb62a8f2a0fe85b4ee95f
kovács dani <kovacs.daniel@avander.hu>
2022-02-25 17:05:23 +0100

    fix

--- tests/basichydro.spec.ts
+++ tests/basichydro.spec.ts
@@ -1,25 +1,11 @@
 import { test, expect } from "@playwright/test"
 
-import { Navbar } from "../page-objects/common/Navbar"
-import { Dashboard } from "../page-objects/common/Dashboard"
-import { AddUserAction } from "../page-objects/common/AddUserAction"
-import { CasePage } from "../page-objects/CasePage"
-//import { secLevels } from "../page-objects/common/constans"
-import { caseType, secLevels, departments } from "../page-objects/common/constans"
 
-import { waitForClass } from "../helpers"
 
 test.describe("my first test suite", () => {
-  let navBar: Navbar
-  let dashBoard: Dashboard
-  let casePage: CasePage
-  let addUserAction: AddUserAction
 
   test.beforeEach(async ({ page }) => {
-    navBar = new Navbar(page)
-    dashBoard = new Dashboard(page)
-    casePage = new CasePage(page)
-    addUserAction = new AddUserAction(page)
+
     const baseUrl = 'https://stage-app-avander-ims-ui.azurewebsites.net'
     await page.screenshot({ path: 'homepage.png' });
     await page.goto(baseUrl)
@@ -28,7 +14,7 @@ test.describe("my first test suite", () => {
 
   test('Add new injury free event', async ({ page }) => {
 
-    await page.pause()
+    //await page.pause()
     //await page.locator(".side-panel-content").waitFor()
     await page.screenshot({ path: 'homepage1.png' });
     await page.locator("#i0116").type("ImsTestGlobalAdmin1@avander.hu")
