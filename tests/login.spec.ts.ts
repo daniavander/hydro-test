3a39421d42439be57fe4d4cd1c21b4e98e5191dc
kovács dani <kovacs.daniel@avander.hu>
2022-03-03 12:48:55 +0100

    fixes

--- tests/login.spec.ts.ts
+++ tests/login.spec.ts.ts
@@ -14,14 +14,15 @@ test.describe("cicd azure simple login test", () => {
     await page.locator('#i0116').type('imstestglobaladmin1@avander.hu')
     await page.keyboard.press('Enter');
     await page.screenshot({ path: 'screenshot1.png' , fullPage: true});
-    await page.waitForSelector('#displayName', {timeout: 30000})
+    await page.waitForSelector('#displayName')
     await page.locator('#i0118').type('123ims456!')
     await page.screenshot({ path: 'screenshot2.png' , fullPage: true});
     await page.keyboard.press('Enter')
-    await page.keyboard.press('Enter')
+    await page.locator('#i0118').type('idSIButton9!')
     await console.log("last step next")
     await page.screenshot({ path: 'screenshot3.png' , fullPage: true});
-    //await page.locator(".top-menu-container").screenshot({ path: 'side-panel-content.png'})
+    await page.pause()
+        //await page.locator(".top-menu-container").screenshot({ path: 'side-panel-content.png'})
   })
 })
 
