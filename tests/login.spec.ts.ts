1c4775c09a9d65bcb610fb4205031bf7986a0d77
kovács dani <kovacs.daniel@avander.hu>
2022-03-03 11:35:30 +0100

    fixes

--- tests/login.spec.ts.ts
+++ tests/login.spec.ts.ts
@@ -10,15 +10,15 @@ test.describe("cicd azure simple login test", () => {
 
   test('cicd azure simple login test', async ({ page }) => {
     //await page.pause()
+    await page.screenshot({ path: './playwright-report/screenshot0.png' , fullPage: true});
     await page.locator('#i0116').type('imstestglobaladmin1@avander.hu')
     await page.keyboard.press('Enter');
     await page.locator('#i0118').type('123ims456!')
     await page.keyboard.press('Enter')
     await page.keyboard.press('Enter')
     await console.log("last step next")
-    await page.screenshot({ path: './test-results/screenshot.png' , fullPage: true});
-    const ghostCard = await page.locator(".top-menu-container")
-    await ghostCard.screenshot({ path: './test-results/side-panel-content.png'})
+    await page.screenshot({ path: './playwright-report/screenshot1.png' , fullPage: true});
+    //const ghostCard = await page.locator(".top-menu-container")
   })
 
 })
