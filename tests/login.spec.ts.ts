50566725673f8e4b6f2ff84c88ad74da2ce3e7db
kovács dani <kovacs.daniel@avander.hu>
2022-03-04 12:47:08 +0100

    fixes

--- tests/login.spec.ts.ts
+++ tests/login.spec.ts.ts
@@ -15,7 +15,8 @@ test.describe("cicd azure describe", () => {
 
   test('cicd azure simple login test', async ({ page }) => {
     await console.log("loog")
-    await page.locator(".dashboard-qr-code-a").screenshot({ path: 'screenhot/side-panel-content.png' })
+    await page.waitForSelector(".dashboard-qr-code-a")
+    await page.locator(".dashboard-qr-code-a").screenshot({ path: 'screenhot/qr-code.png' })
   })
 
   test('cicd expect', async ({ page }) => {
