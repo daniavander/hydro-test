4aee145c524a1d5c23562233acbc8eea8abb071d
Dongó <kovacs.daniel@avander.hu>
2022-03-04 15:40:17 +0100

    activities

--- tests/smoke/activityList.spec.ts
+++ tests/smoke/activityList.spec.ts
@@ -8,13 +8,13 @@ test.describe("Smoke test - Activity list", () => {
   let navBar: Navbar
   let dashBoard: Dashboard
 
-
+  const baseUrl = 'https://stage-app-avander-ims-ui.azurewebsites.net/app/'
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page)
     navBar = new Navbar(page)
     dashBoard = new Dashboard(page)
-    const baseUrl = 'https://stage-app-avander-ims-ui.azurewebsites.net/app/'
+
     await page.goto(baseUrl, { timeout: 50000 })
     await loginPage.loginInAzure()
   })
@@ -24,7 +24,16 @@ test.describe("Smoke test - Activity list", () => {
     await dashBoard.topBarIsAvailable()
     //await page.pause()
     await expect(page.locator("#filter-site")).toHaveAttribute('title', 'All MY sites')
+    //expect(await page.locator(".side-panel-content").screenshot()).toMatchSnapshot('side-panel-content.png', { threshold: 0.5 })
+    await navBar.clickOnTopMenu("Activities")
 
+    await page.locator('.obs_csstable').isVisible()
+    const activitiesHeader = page.locator('.header.header-style2');
+    await expect(activitiesHeader).toHaveClass("row obs_flex obs_flexgrow1 header header-style2");
+    expect(await activitiesHeader.screenshot()).toMatchSnapshot('activitie-list-header.png', { threshold: 0.5 })
+
+    const response = await request.get(`${baseUrl}pi/activity?queryString=`)
+    expect(response.status()).toBe(200)
 
   })
 })
