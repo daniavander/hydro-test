1ec2609023c9c24dc7878fdd61d8e9f9b1a5a79c
Dongó <kovacs.daniel@avander.hu>
2022-03-07 12:26:06 +0100

    fixes

--- tests/login.spec.ts.ts
+++ tests/login.spec.ts.ts
@@ -42,14 +42,14 @@ test.describe("cicd azure describe", () => {
     //await delay(5000);
     await page.screenshot({ path: 'screenshot/screenshot3.png' });
 
-    /*const ghostCard = await page.locator(".side-panel-content")
+    const ghostCard = await page.locator(".side-panel-content")
     await ghostCard.screenshot({ path: 'screenshot/side-panel-content.png' })
 
     console.log('before waiting');
     await ghostCard.screenshot({ path: 'screenshot/side-panel-content1.png' })
     await delay(15000);
     console.log('after waiting 15 sec');
-    await ghostCard.screenshot({ path: 'screenshot/side-panel-content2.png' })*/
+    await ghostCard.screenshot({ path: 'screenshot/side-panel-content2.png' })
 
     //await this.page.locator(".top-menu-container").screenshot({ path: 'screenhot/header.png' })
     //await page.waitForSelector(".dashboard-qr-code-a")
