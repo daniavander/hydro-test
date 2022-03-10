92769007441945bab3ba80be5ddc0769222e2edf
Dongó <kovacs.daniel@avander.hu>
2022-03-10 12:51:24 +0100

    useraction

--- page-objects/common/AddUserAction.ts
+++ page-objects/common/AddUserAction.ts
@@ -20,12 +20,12 @@ export class AddUserAction {
         await this.instruction.type(msg)
     }
 
-    async addResponsible(loc: string) {
+    async addResponsible(name: string) {
         //loc: Kovács Dániel(kovacs.daniel@avander.hu)
         //ha placeholder van akkor csak egy \ a space előtt !!!!!!!!!!!!!!!!!!!!!!!!!!!
-        await this.page.locator('[placeholder="Add\ responsible"]').click()
+        await this.page.fill('[placeholder="Add\ responsible"]','kovács dá')
         //fixme faszért nem megy paraméterrel
-        await this.page.locator('text=Kovács Dániel(kovacs.daniel@avander.hu)').click();
+        await this.page.locator('//div[text()="(' + name + ')"]').click();
         //await this.page.locator("text=" + loc + "]").click();
         //await this.page.locator('text=Kovács Dániel(kovacs.daniel@avander.hu)').click();
     }
@@ -43,18 +43,11 @@ export class AddUserAction {
 
 
     async addNewAction(desc: string, inst: string, name: string, main: string, sub: string) {
-        /*
-        await page.locator("//span[text()='Add new action']").click()
-    await addUserAction.addDescription("lorem ipsom dolor sit amen")
-    await addUserAction.addInstruction("instruction")
-    await addUserAction.addResponsible("Kovács\\ Dániel(kovacs.daniel@avander.hu)")
-    await addUserAction.addTags("Add Action tag", "action1")
-    */
         await this.page.locator("//span[text()='Add new action']").click()
         await this.page.locator("(//textarea)[2]").type(desc)
         await this.instruction.type(inst)
-        await this.page.locator('[placeholder="Add\ responsible"]').click()
-        await this.page.locator("//div[text()='" + name + "']").click();
+        await this.page.fill('[placeholder="Add\ responsible"]','kovács dá')
+        await this.page.locator("//div[text()='(" + name + ")']").click();
         await this.page.locator("//span[text()='" + main + "']").click();
         await this.page.locator("[aria-label=" + sub + "]").click();
         await this.page.locator("//button[text()='Save changes']").click()
