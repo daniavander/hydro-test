7c51023dc56eb5012f7c5ae2c3ff95286b63505e
Dongó <kovacs.daniel@avander.hu>
2022-03-10 09:11:34 +0100

    addife

--- tests/ife.spec.ts
+++ tests/ife.spec.ts
@@ -4,24 +4,32 @@ import { Navbar } from "../page-objects/common/Navbar"
 import { Dashboard } from "../page-objects/common/Dashboard"
 import { AddUserAction } from "../page-objects/common/AddUserAction"
 import { CasePage } from "../page-objects/CasePage"
+import { CaseList } from "../page-objects/CaseList"
+import { LoginPage } from "../page-objects/LoginPage"
 //import { secLevels } from "../page-objects/common/constans"
 import { caseType, secLevels, departments } from "../page-objects/common/constans"
 
-import { waitForClass } from "../helpers"
+//import { waitForClass } from "../helpers"
 
 test.describe("my first test suite", () => {
   let navBar: Navbar
   let dashBoard: Dashboard
   let casePage: CasePage
+  let caseList: CaseList
   let addUserAction: AddUserAction
+  let loginPage: LoginPage
 
   test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page)
+
     navBar = new Navbar(page)
     dashBoard = new Dashboard(page)
     casePage = new CasePage(page)
+    caseList = new CaseList(page)
     addUserAction = new AddUserAction(page)
     const baseUrl = 'https://stage-app-avander-ims-ui.azurewebsites.net/app/'
     await page.goto(baseUrl, { timeout: 50000 })
+    await loginPage.loginInAzure()
   })
 
   test('Add new injury free event', async ({ page }) => {
@@ -45,6 +53,7 @@ test.describe("my first test suite", () => {
     await casePage.fillDescription("lorem ipsum set dolor sit amen")
 
     await casePage.addMainAndSubTag("Add Csilla teszt", "Csilla2")
+    await casePage.addMainAndSubTagWithoutBtn("Add Műszak meghatározása", "Nappali műszak")
 
     await page.click("//button[text()='Save']")
 
@@ -53,12 +62,15 @@ test.describe("my first test suite", () => {
     await page.pause()
     //await casePage.snapshotGhostCard()
 
-    await addUserAction.addNewAction("description","instruction","Kovács Dániel","Add Action tag", "action1")
+   // await addUserAction.addNewAction("description","instruction","Kovács Dániel","Add Action tag", "action1")
 
-    await casePage.pageContainsActionCorrectly("description","instruction")
+    //await casePage.pageContainsActionCorrectly("description","instruction")
 
-    const locator = page.locator('.fullopacity');
-    await expect(locator).toHaveClass("tile fadein action list-mode ng-star-inserted fullopacity my-task active");
+    //const locator = page.locator('.fullopacity');
+    //await expect(locator).toHaveClass("tile fadein action list-mode ng-star-inserted fullopacity my-task active");
+    await page.locator('text=Close').click();
+
+    await caseList.getCaseByDescriptionAndDo("lorem ipsum set dolor sit amen","Delete")
   })
 })
 
